a6a2f134b73540952dccf1850292be30950647a4
Juan Diego Jimenez <jdiejim@gmail.com>
2017-10-31 17:00:46 -0600

    Add toggle menu for mobile design

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,25 @@ import Header from './Header';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuIsHidden: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({ menuIsHidden: !this.state.menuIsHidden });
+  }
+
   render() {
+    const { menuIsHidden } = this.state;
+
     return (
       <section className="App">
-        <Header />
+        <Header handleOnClick={this.toggleMenu} menuIsHidden={menuIsHidden} />
       </section>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import menu from '../assets/menu.svg';
+import cancel from '../assets/cancel.svg';
 import './styles/Header.css';
 
-function Header() {
+function Header({ handleOnClick, menuIsHidden }) {
+  const menuBG = { backgroundImage: `url(${menu})` };
+  const cancelBG = { backgroundImage: `url(${cancel})` };
+  const headerClass = `Header ${menuIsHidden ? 'menu-hidden' : ''}`
+
   return (
-    <header className="Header">
+    <header className={headerClass}>
+      <button onClick={handleOnClick} className="Header-btn-menu" style={menuBG} />
        <nav className="Header-nav">
+          <button onClick={handleOnClick} className="Header-btn-cancel" style={cancelBG} />
           <NavLink to="/about" className="Header-tab" activeClassName="is-selected">About</NavLink>
           <NavLink to="/projects" className="Header-tab" activeClassName="is-selected">Projects</NavLink>
           <NavLink to="/skills" className="Header-tab" activeClassName="is-selected">Skills</NavLink>
-          <NavLink to="/skills" className="Header-tab" activeClassName="is-selected">Blog</NavLink>
-          <button id="contact-me">Contact Me</button>
+          <NavLink to="/blog" className="Header-tab" activeClassName="is-selected">Blog</NavLink>
       </nav>
+      <button id="contact-me">Contact Me</button>
     </header>
   );
 }
